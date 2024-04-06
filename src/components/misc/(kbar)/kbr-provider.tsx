3a94e5a18535119b5829abcb12ae7f1864806053
falcon71181 <abhaythakur71181@gmail.com>
2024-04-06 11:14:38 +0530

    kbar provider

---
+++ src/components/misc/(kbar)/kbr-provider.tsx
@@ -0,0 +1,54 @@
+import { ReactNode } from "react";
+import { KBarProvider, Action } from "kbar";
+import { useTheme } from "next-themes";
+import { Monitor, Palette, Sun, Moon } from "lucide-react";
+
+export const CustomKbarProvider = ({ children }: { children: ReactNode }) => {
+  const { setTheme } = useTheme();
+  const iconSize = "1rem";
+
+  const themeActions: Action[] = [
+    {
+      id: "set-theme",
+      name: "Change Theme",
+      icon: <Palette size={iconSize} />,
+      shortcut: ['c', 't'],
+      keywords: "theme system dark light",
+      section: "Configurations"
+    },
+    {
+      id: "system-theme",
+      name: "System theme",
+      icon: <Monitor size={iconSize} />,
+      parent: "set-theme",
+      keywords: 'theme system',
+      perform: () => setTheme("system")
+    },
+    {
+      id: "dark-theme",
+      name: "Dark theme",
+      icon: <Moon size={iconSize} />,
+      parent: "set-theme",
+      keywords: 'theme dark',
+      perform: () => setTheme("dark")
+    },
+    {
+      id: "light-theme",
+      name: "Light theme",
+      icon: <Sun size={iconSize} />,
+      parent: "set-theme",
+      keywords: 'theme light',
+      perform: () => setTheme("light")
+    },
+  ]
+
+  const actions: Action[] = [
+    ...themeActions,
+  ]
+
+  return (
+    <KBarProvider actions={actions} >
+      {children}
+    </KBarProvider>
+  )
+}
