baf566316a29c5c0c5ea9d0cb4b0a839a6c5c4f1
falcon71181 <abhaythakur71181@gmail.com>
2024-06-30 20:49:29 +0530

    feat: Translate Log Out Button added

--- src/components/ui-group/ButtonCards.tsx
+++ src/components/ui-group/ButtonCards.tsx
@@ -1,12 +1,18 @@
 import ComponentGrid from "./ComponentGrid";
 import type { ComponentDataType } from "@/types/component";
 import HeartbeatButton from "@/app/ui/components/(components)/buttons/HeartbeatButton";
+import TranslateLogOutButton from "@/app/ui/components/(components)/buttons/TranslateLogOutButton";
 
 const buttons: ComponentDataType[] = [
   {
     title: "Heart Beat",
     link: '/ui/components/button/heartbeat-button',
     component: <HeartbeatButton />
+  },
+  {
+    title: "Translate Log Out",
+    link: '/ui/components/button/translate-log-out-button',
+    component: <TranslateLogOutButton />
   },
 ]
 
